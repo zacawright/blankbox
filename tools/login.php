0f744586565c7acc303f029cfce31bd737adf76d
zacawright <zacawright@gmail.com>
2017-03-07 14:13:52 +0000

    Pre Merge Commit - Fixed Incomplete Class

--- tools/login.php
+++ tools/login.php
@@ -2,7 +2,7 @@
     require_once("../utilities/connect.php");
     require_once("../utilities/usermodel.php");
     
-    
+    session_start();
 //public static function checkLogin($username, $password) {
 
     if(isset($_POST['username']) && isset($_POST['password'])) {
@@ -19,7 +19,7 @@
         $SQL_QUERY_LOGIN_CHECK_ROWS = $SQL_QUERY_LOGIN_CHECK->num_rows;
         
         if($SQL_QUERY_LOGIN_CHECK_ROWS == 1) {
-            $_SESSION['activeuser'] = User::fromUsername($USERNAME_LOWER);
+            $_SESSION['activeuser'] = UserClass::fromUsername($USERNAME_LOWER);
             $connection->close();
             header("location: ../index.php");
             
---
+++ tools/register.php
@@ -0,0 +1,10 @@
+<?php
+    require_once("../utilities/connect.php");
+    require_once("../utilities/usermodel.php");
+    
+    session_start();
+    
+    if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
+        
+    }
+?>
